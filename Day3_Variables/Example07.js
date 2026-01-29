aaa2c807db0fea7e59242d7fe9fb777b3641d5c8
Ravi Kapadia <139764414+rvikapadia9@users.noreply.github.com>
2026-01-29 11:47:45 +0530

    Javascript Exercise

---
+++ Day3_Variables/Example07.js
@@ -0,0 +1,8 @@
+// Create a variable age and assign a value.
+// Update the value and print both before and after.
+
+let age = 25;
+console.log("Age before update:", age); 
+
+age = 26;
+console.log("Age after update:", age);  
---
+++ Day3_Variables/Example14.js
@@ -0,0 +1,31 @@
+// Boolean Variable
+
+// Create variables:
+
+// isLoggedIn
+
+// hasPermission
+
+// Assign boolean values and print meaningful messages based on them.
+
+// Goal: Understand boolean variables
+"use strict";
+let isLoggedIn = true;
+let hasPermission = false;
+if (isLoggedIn) {
+  console.log("User is logged in.");
+}
+if (hasPermission) {
+    console.log("User has permission.");
+} else {
+    console.log("User does not have permission.");
+}
+// Expected Output:
+// User is logged in.
+// User does not have permission.  
+
+
+
+
+
+        
