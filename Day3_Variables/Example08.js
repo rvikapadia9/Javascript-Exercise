aab7227e61353d030306135d6d06562aed479dc3
Ravi Kapadia <139764414+rvikapadia9@users.noreply.github.com>
2026-01-29 11:47:45 +0530

    Fix: remove submodule and add Day3_Variables as normal folder

---
+++ Day3_Variables/Example08.js
@@ -0,0 +1,15 @@
+// Multiple Variables
+
+// Create variables for:
+
+// firstName
+
+// lastName
+
+// city
+
+// Print a sentence using all of them.
+let firstName = "Ravi";
+let lastName = "Kapadia";
+let city = "Ahmedabad";
+console.log("My name is " + firstName + " " + lastName + " and I live in " + city + ".");
---
+++ Day3_Variables/Example15.js
@@ -0,0 +1,25 @@
+// Mini Real-Life Scenario
+
+// Store these details using variables:
+
+// employee name
+
+// working hours per day
+
+// hourly rate
+
+// Calculate and print daily salary.
+"use strict";
+let employeeName = "Ravi Kapadia";
+let workingHoursPerDay = 8;
+let hourlyRate = 150;
+let dailySalary = workingHoursPerDay * hourlyRate;
+console.log("Employee Name:", employeeName);
+console.log("Daily Salary:", dailySalary);  
+// Expected Output:
+// Employee Name: Ravi Kapadia
+// Daily Salary: 1200   
+
+
+
+    
