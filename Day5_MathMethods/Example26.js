8921b3d68585ed0f4ec31485ee8654c682f4ac50
Ravi Kapadia - DevRepublic <ravik@devrepublic.nl>
2026-02-04 11:30:20 +0530

    Decision making-If statement

--- Day5_MathMethods/Example26.js
+++ Day5_MathMethods/Example26.js
@@ -4,7 +4,10 @@
 
 // Print the result
 let num1 = 15;
+
 let num2 = 27;
+
 let sum = num1 + num2;
 console.log("The sum of", num1, "and", num2, "is:", sum);
 
+    
---
+++ Day7_if_statement/Example53.js
@@ -0,0 +1,9 @@
+// Print "Access denied" if user is NOT an admin.
+let isAdmin = false; // You can change this value to test different cases
+if (!isAdmin) {
+    console.log("Access denied");
+}
+// If isAdmin is true, nothing will be printed.
+// Example Outputs:
+// For isAdmin = false, Output: Access denied
+// For isAdmin = true, Output: (no output)  
