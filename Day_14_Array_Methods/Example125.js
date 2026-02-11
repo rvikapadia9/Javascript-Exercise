63c50fa2740ea86fdb2fa4c084bb83c05ff3937e
Ravi kapadia <rvkapadia9@gmail.com>
2026-02-11 11:48:15 +0530

    feat: Add `indexOf()` array method example to demonstrate finding element indices.

--- Day_14_Array_Methods/Example125.js
+++ Day_14_Array_Methods/Example125.js
@@ -11,5 +11,7 @@
 
 let fruits = ["apple", "banana", "orange"];
 
-console.log(fruits.indexOf("banana")); // 1
+console.log(fruits.indexOf("banana")); // console.log(fruits.indexOf("grapes")); // -1 (not found)
 console.log(fruits.indexOf("grapes")); // -1 (not found)
+//explanation
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
