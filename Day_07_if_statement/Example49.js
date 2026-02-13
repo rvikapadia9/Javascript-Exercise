23b97c2ad47b8d55a2daca71958f7af88649313a
Ravi kapadia <rvkapadia9@gmail.com>
2026-02-13 11:37:32 +0530

    Fix case-insensitive duplicate folders (Day_07, Day_08)

--- Day_07_if_statement/Example49.js
+++
@@ -1,5 +0,0 @@
-// Print "Username provided" only if username is not empty.
-let username = "Ravi"; // You can change this value to test different cases   
-if (username !== "") {
-    console.log("Username provided");
-}
--- Day_07_if_statement/Example50.js
+++
@@ -1,9 +0,0 @@
-// Print "Even number" only if the number is even.
-let number = 4; // You can change this value to test different cases
-if (number % 2 === 0) {
-    console.log("Even number");
-}
-// If the number is not even, nothing will be printed.
-// Example Outputs:
-// For number = 4, Output: Even number
-// For number = 7, Output: (no output)
